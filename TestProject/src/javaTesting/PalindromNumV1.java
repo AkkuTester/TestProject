560786c6b3ed3f378d6d0816cc972ef84bdd06a0
HP <HP@DESKTOP-BTQ2T4D.lan>
2023-01-14 10:18:18 +0530

    PalindromProgram

---
+++ TestProject/src/javaTesting/PalindromNumV1.java
@@ -0,0 +1,12 @@
+package javaTesting;
+
+public class PalindromNumV1 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("here we will have the program for finding whether the number is palindrom or not");
+		
+
+	}
+
+}
