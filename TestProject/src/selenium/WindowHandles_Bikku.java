6b5438d1223b8df1d445a4ac4f6845b2662145b3
HP <HP@DESKTOP-BTQ2T4D.lan>
2023-01-14 11:32:02 +0530

    windowsHnadles committed by bikuu

---
+++ TestProject/src/selenium/WindowHandles_Bikku.java
@@ -0,0 +1,12 @@
+package selenium;
+
+public class WindowHandles_Bikku {
+
+	//// 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
+	public static void main(String[] args) {
+	
+		//code by bikku for the window handles
+
+	}
+
+}
